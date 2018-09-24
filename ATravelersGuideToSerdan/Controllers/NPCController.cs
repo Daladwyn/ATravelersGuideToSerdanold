39a452e1e559129a2674d161095f4af7a9ccb6ba
Christian Levin <christian.johansson@gmail.com>
2018-09-24 16:28:33 +0200

    started to build the generations view

--- ATravelersGuideToSerdan/Controllers/NPCController.cs
+++ ATravelersGuideToSerdan/Controllers/NPCController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATravelersGuideToSerdan.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,33 @@ namespace ATravelersGuideToSerdan.Controllers
 {
     public class NPCController : Controller
     {
+        private SerdanDb Db;
+
+        public NPCController()
+        {
+            Db = new SerdanDb();
+        }
+
         // GET: NPC
-        public ActionResult Index()
+        public ActionResult NPCIndex()
         {
             return View();
+        }
+
+        public ActionResult GetNpcs(string type)
+        {
+            List<NPC> ListOfNPCs = Db.NPCs.ToList();
+            List<NPC> RequestedTypeOfNPCs = new List<NPC>();
+            foreach (var NPC in ListOfNPCs)
+            {
+                if (NPC.NpcFamily==type)
+                {
+                    RequestedTypeOfNPCs.Add(NPC);
+                }
+            }
+            
+            
+            return View("GetNpcs", RequestedTypeOfNPCs);
         }
     }
 }
---
+++ ATravelersGuideToSerdan/Models/ViewModels/NPCGenerationViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATravelersGuideToSerdan.Models.ViewModels
+{
+    public class NPCGenerationViewModel
+    {
+        [Required]
+        public int NpcId { get; set; }
+
+        [Range(0,7)]
+        public byte NPCGeneration { get; set; }
+
+        public string NpcName { get; set; }
+
+    }
+
+}
