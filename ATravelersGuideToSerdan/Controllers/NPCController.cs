6db5a874bfc35b3ad93ea39b9c44d3d0dc56fe89
Christian Levin <christian.johansson@gmail.com>
2018-10-09 16:33:08 +0200

    actionresults are created for viewing Npc stats

--- ATravelersGuideToSerdan/Controllers/NPCController.cs
+++ ATravelersGuideToSerdan/Controllers/NPCController.cs
@@ -30,23 +30,54 @@ namespace ATravelersGuideToSerdan.Controllers
             List<NPC> RequestedTypeOfNPCs = new List<NPC>();
             foreach (var NPC in ListOfNPCs)
             {
-                if (NPC.NpcFamily==type)
+                if (NPC.NpcFamily == type)
                 {
                     RequestedTypeOfNPCs.Add(NPC);
                 }
             }
-            
-            
+
+
             return View("_GetNpcList", RequestedTypeOfNPCs);
 
 
         }
 
-        public ActionResult GetNpc (int Id)
+        public ActionResult GetNpc(int Id)
         {
             NPCCompleteStatsViewModel ASpecificNpcData = new NPCCompleteStatsViewModel();
 
             return PartialView("_SpecificNpc", ASpecificNpcData);
+        }
+
+        public ActionResult GetStats(int Id)
+        {
+            NpcStatsViewModel NpcStats = new NpcStatsViewModel();
+
+            return PartialView("_GetStats", NpcStats);
+        }
+
+        public ActionResult GetApperance(int Id)
+        {
+            NpcApperanceViewModel NpcApperance = new NpcApperanceViewModel();
+            return PartialView("_GetApperance", NpcApperance);
+        }
+
+        public ActionResult GetRelatives(int Id)
+        {
+            NpcRelativesViewModel NpcRelatives = new NpcRelativesViewModel();
+            return PartialView("_GetRelatives", NpcRelatives);
+        }
+
+        public ActionResult GetOthers(int Id)
+        {
+            NpcOthersViewModel NpcOthers = new NpcOthersViewModel();
+            return PartialView("_GetOthers", NpcOthers);
+        }
+
+        public ActionResult GetDiverse(int Id)
+        {
+            NpcDiverseViewModel NpcDiverse = new NpcDiverseViewModel();
+            return PartialView("_GetDiverse", NpcDiverse);
         }
     }
 }
