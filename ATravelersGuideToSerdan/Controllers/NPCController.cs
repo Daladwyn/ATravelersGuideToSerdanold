240ef069c93d0b10b3bbe5c4236e685a190e49d0
Christian Levin <christian.johansson@gmail.com>
2018-10-11 15:01:49 +0200

    actionresullts to present npc data regarding relatives and other personsFactions is made

--- ATravelersGuideToSerdan/Controllers/NPCController.cs
+++ ATravelersGuideToSerdan/Controllers/NPCController.cs
@@ -64,13 +64,38 @@ namespace ATravelersGuideToSerdan.Controllers
 
         public ActionResult GetRelatives(int Id)
         {
+            NPC NpcToFetch = Db.NPCs.SingleOrDefault(i => i.NpcId == Id);
+            if (NpcToFetch == null)
+            {
+                return PartialView("_GetRelatives");
+            }
             NpcRelativesViewModel NpcRelatives = new NpcRelativesViewModel();
+            NpcRelatives.NpcId = NpcToFetch.NpcId;
+            NpcRelatives.NpcsFather = NpcToFetch.NpcsFather;
+            NpcRelatives.NpcsMother = NpcToFetch.NpcsMother;
+            NpcRelatives.NpcsRegularChildren = NpcToFetch.NpcsRegularChildren;
+            NpcRelatives.NpcsSerdanEdlosiChildren = NpcToFetch.NpcsSerdanEdlosiChildren;
             return PartialView("_GetRelatives", NpcRelatives);
         }
 
         public ActionResult GetOthers(int Id)
         {
+            NPC NpcToFetch = Db.NPCs.SingleOrDefault(i => i.NpcId == Id);
+            if (NpcToFetch == null)
+            {
+                return PartialView("_GetOthers");
+            }
             NpcOthersViewModel NpcOthers = new NpcOthersViewModel();
+            NpcOthers.NpcId = NpcToFetch.NpcId;
+            NpcOthers.NpcAllies = NpcToFetch.NpcAllies;
+            NpcOthers.NpcEnemies = NpcToFetch.NpcEnemies;
+            NpcOthers.NpcFriends = NpcToFetch.NpcFriends;
+            NpcOthers.NpcRegardingAets = NpcToFetch.NpcRegardingAets;
+            NpcOthers.NpcRegardingDemons = NpcToFetch.NpcRegardingDemons;
+            NpcOthers.NpcRegardingDragons = NpcToFetch.NpcRegardingDragons;
+            NpcOthers.NpcRegardingGrips = NpcToFetch.NpcRegardingGrips;
+            NpcOthers.NpcRegardingOthers = NpcToFetch.NpcRegardingOthers;
+            NpcOthers.NpcRivals = NpcToFetch.NpcRivals;
             return PartialView("_GetOthers", NpcOthers);
         }
 
