c16b7312677918a9ae94ee737c9700cd00b00020
Christian Levin <christian.johansson@gmail.com>
2018-10-21 00:05:40 +0200

    displaying relatives to the Npc is made.

---
+++ ATravelersGuideToSerdan/Models/ViewModels/NpcListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ATravelersGuideToSerdan.Models.ViewModels
+{
+    public class NpcListViewModel
+    {
+        [Required]
+        public int NpcId { get; set; }
+
+        [Required]
+        public string NpcName { get; set; }
+    }
+}
--- ATravelersGuideToSerdan/Models/ViewModels/NpcRelativesViewModel.cs
+++ ATravelersGuideToSerdan/Models/ViewModels/NpcRelativesViewModel.cs
@@ -6,36 +6,60 @@ using System.Web;
 
 namespace ATravelersGuideToSerdan.Models.ViewModels
 {
+
     public class NpcRelativesViewModel
     {
         [Required]
         public int NpcId { get; set; }
 
         [Display(Name = "Mor")]
-        public int NpcsMother { get; set; }
+        public string NpcsMother { get; set; }
 
         [Display(Name = "Far")]
-        public int NpcsFather { get; set; }
+        public string NpcsFather { get; set; }
 
         [Display(Name = "Serdan/Edlosi barn")]
-        public List<int> NpcsSerdanEdlosiChildren { get; set; }
+        public List<NpcListViewModel> NpcsSerdanEdlosiChildren { get; set; }
 
         [Display(Name = "Vanliga Barn")]
-        public List<int> NpcsRegularChildren { get; set; }
+        public List<NpcListViewModel> NpcsRegularChildren { get; set; }
 
 
         internal static NpcRelativesViewModel AssignRelativesData(NPC NpcToAssign)
         {
+            SerdanDb Db = new SerdanDb();
+            NPC NpcFather = Db.NPCs.SingleOrDefault(n => n.NpcId == NpcToAssign.NpcsFather);
+            NPC NpcMother = Db.NPCs.SingleOrDefault(n => n.NpcId == NpcToAssign.NpcsMother);
             NpcRelativesViewModel FilteredNpc = new NpcRelativesViewModel
             {
                 NpcId = NpcToAssign.NpcId,
-                NpcsFather = NpcToAssign.NpcsFather,
-                NpcsMother = NpcToAssign.NpcsMother,
-                NpcsRegularChildren = NpcToAssign.NpcsRegularChildren,
-                NpcsSerdanEdlosiChildren = NpcToAssign.NpcsSerdanEdlosiChildren,
-
+                NpcsFather = NpcFather == null ? "Okänd." : NpcFather.NpcName,
+                NpcsMother = NpcMother == null ? "Okänd." : NpcMother.NpcName,
+                NpcsRegularChildren = GatherTheChildren(NpcToAssign.NpcsRegularChildren),
+                NpcsSerdanEdlosiChildren = GatherTheChildren(NpcToAssign.NpcsSerdanEdlosiChildren)
             };
             return FilteredNpc;
+        }
+        /// <summary>
+        /// Function that returns a list of NpcId and NpcName from a list of NpcIds.
+        /// </summary>
+        /// <param name="ListOfNumbers"> This is the list from which the children is collected.</param>
+        /// <returns></returns>
+        static List<NpcListViewModel> GatherTheChildren(List<int> ListOfNumbers)
+        {
+            SerdanDb Db = new SerdanDb();
+            List<NpcListViewModel> childList = new List<NpcListViewModel>();
+            for (int i = 0; i < ListOfNumbers.Capacity; i++)
+            {
+                NPC Child = Db.NPCs.SingleOrDefault(n => n.NpcId == ListOfNumbers[i]);
+                NpcListViewModel compactChild = new NpcListViewModel
+                {
+                    NpcId = Child.NpcId,
+                    NpcName = Child.NpcName
+                };
+                childList.Add(compactChild);
+            }
+            return childList;
         }
     }
 }
